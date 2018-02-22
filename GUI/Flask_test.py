20c08ab9c710cb909a6ac218fa3a45eebfd1a00b
lizavieta <lizavieta.fedorko@gmail.com>
2018-02-22 17:24:35 +0200

    add templates for option

--- GUI/Flask_test.py
+++ GUI/Flask_test.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Lalalala'
 
-@app.route('/option')
-def interface():
+@app.route('/')
+def my_form():
     return render_template('option.html')
 
-if __name__ == "__main__": ##run app
-    app.run(debug=True) ##start server
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['P_XS']
+    processed_text = text.upper()
+    print(processed_text)
+    return processed_text
+
+if __name__ == "__main__":
+    app.run(debug=True)
