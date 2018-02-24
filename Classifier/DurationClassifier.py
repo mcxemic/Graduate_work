b29577f16e82e70b818ebad2a3073bdc389e12a3
mexenic <mcxemic@gmail.com>
2018-02-24 14:09:20 +0200

    reorganized structure, add model&database connection
    Signed-off-by: mexenic <mcxemic@gmail.com>

--- Classifier/DurationClassifier.py
+++
@@ -1,9 +0,0 @@
-class DurationClassifier():
-    # Class generate duration for task
-
-    def __init__(self, XS=5, S=10, M=20, L=50, XL=100):
-        self.XS = XS
-        self.S = S
-        self.M = M
-        self.L = L
-        self.XL = XL
