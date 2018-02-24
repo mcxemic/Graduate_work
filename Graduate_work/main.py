769dc1e36cfd5b1c4f3735245664832d4118375c
Lizavieta Fedorko <lizavieta.fedorko@gmail.com>
2018-02-24 18:50:51 +0200

    add option form basic

--- Graduate_work/main.py
+++ Graduate_work/main.py
@@ -6,10 +6,13 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from wtforms import IntegerField, SelectField
+from wtforms.validators import NumberRange,InputRequired
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET'
 
 POSTGRES = {
     'user': 'IS_42',
@@ -60,7 +63,6 @@ class Set(db.Model):
 
     child = db.relationship("Task")
 
-
     # TODO __repr__
 
 
@@ -104,5 +106,39 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # TODO add classifier interaction
-    return render_template("interface.html")
+    form = MainForm()
+    return render_template("interface.html", form=form)
+
+
+@app.route('/options', methods=['GET', 'POST'])
+def options():
+    form = OptionForm()
+    return render_template("options.html", form=form)
+
+
+######FORM CLASSES
+class MainForm(FlaskForm):
+    P = SelectField(u'Programming Language', choices=[(1, 'XS'), (2, 'S'), (2, 'M'), (2, 'L'), (2, 'XL')])
+    Q = SelectField(u'Programming Language', choices=[(1, 'XS'), (2, 'S'), (2, 'M'), (2, 'L'), (2, 'XL')])
+    H = SelectField(u'Programming Language', choices=[(1, 'XS'), (2, 'S'), (2, 'M'), (2, 'L'), (2, 'XL')])
+
+class OptionForm(FlaskForm):
+    P_XS = IntegerField('XS', validators=[InputRequired(), NumberRange(1, 100)])
+    P_S = IntegerField('S ')
+    P_M = IntegerField('M ')
+    P_L = IntegerField('L ')
+    P_XL = IntegerField('XL')
+    Q_XS = IntegerField('XS')
+    Q_S = IntegerField('S ')
+    Q_M = IntegerField('M ')
+    Q_L = IntegerField('L ')
+    Q_XL = IntegerField('XL')
+    H_XS = IntegerField('XS')
+    H_S = IntegerField('S ')
+    H_M = IntegerField('M ')
+    H_L = IntegerField('L ')
+    H_XL = IntegerField('XL')
+    C = IntegerField('C ')
+
+if __name__ == "__main__":
+    app.run(debug=True)
