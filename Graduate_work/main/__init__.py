1976ff33483e025445c58ef4c549d139fd102de0
mexenic <mcxemic@gmail.com>
2018-04-24 13:19:38 +0300

    add second gen_algo
    Signed-off-by: mexenic <mcxemic@gmail.com>

--- Graduate_work/main/__init__.py
+++ Graduate_work/main/__init__.py
@@ -20,33 +20,32 @@ def generate_sets(type_distribution, count_set, count_devices, mean_duration_P, 
 def create_task_for_multiply_machine(type_distribution, count_devices, mu, sigma, C, gen_algo):
     sets_of_machine = []
     for _ in range(count_devices):
-        sets_of_machine.extend(create_task_for_one_machine(type_distribution, mu, sigma, C))
+        sets_of_machine.extend(create_task_for_one_machine(type_distribution, mu, sigma, C, gen_algo))
     sets_of_machine.sort()
     return sets_of_machine
 
 
-def create_task_for_one_machine(type_distribution, mu, sigma, c):
-    print("create task for one machine", type_distribution, mu, sigma, c)
-
+def create_task_for_one_machine(type_distribution, mu, sigma, c, gen_algo):
+    import random
     normal_distribution_set = choose_distribution(type_distribution, mu, sigma, c)
     machine = [int(i) for i in normal_distribution_set]
     if sum(machine) < c:
         machine.append(c - sum(machine))
+
+    if random.randint(0, 1) == 1 and gen_algo == '2':
+        machine.append(1)
     machine.sort()
     return machine
 
 
 def create_set_distribution(method, mu, sigma, c):
-    import random
-
     normal = []
     while c > mu + 2 * sigma:
         x = method(mu, sigma)
         normal.append(x)
         c -= x
     normal.append(c)
-    if random.randint(0, 1) == 1:
-        normal.append(1)
+
     return normal
 
 
